411c2fa5ce4d7c3cb798ad51aa655fbeb07d752e
김태희 <evan@Taehees-MacBook-Pro.local>
2023-02-20 19:29:46 +0900

    P.240 자바의 신

---
+++ src/main/java/extends240/Main.java
@@ -0,0 +1,16 @@
+package extends240;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Parent parent = new Parent();
+
+        Child child = new Child();
+
+        // Log
+        // Parent Constructor
+        // Parent Constructor
+        // Child Constructor
+
+    }
+}
---
+++ src/main/java/extends240/Parent.java
@@ -0,0 +1,12 @@
+package extends240;
+
+public class Parent {
+
+    public Parent() {
+        System.out.println("Parent Constructor");
+    }
+
+    public void printName() {
+        System.out.println("Parent printName()");
+    }
+}
