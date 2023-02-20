56d898657ebc4ee43be4a0c55839658dde93a499
김태희 <evan@Taehees-MacBook-Pro.local>
2023-02-20 19:30:57 +0900

    P.241 자바의 신

---
+++ src/main/java/extends240/InheritancePrint.java
@@ -0,0 +1,13 @@
+package extends240;
+
+public class InheritancePrint {
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.printName();
+
+        // Log
+        // Parent Constructor
+        // Child Constructor
+        // Parent printName()
+    }
+}
